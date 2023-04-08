c97fd27092261600d807cf9f7573a1c94c727772
Prakash-Jha-Dev <only2prakash@gmail.com>
2023-04-08 06:51:49 +0530

    PubSub HLD

---
+++ publish-subscribe/src/test/java/com/ha/publishsubscribe/PublishSubscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha.publishsubscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishSubscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
